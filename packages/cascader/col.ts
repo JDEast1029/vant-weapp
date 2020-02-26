f2c6bd6d7e5e36b6a5ae8a819d94322b28517e1c
yanbingbin <785175917@qq.com>
2020-02-26 16:14:38 +0800

    fix: cascader

--- packages/cascader/col.ts
+++ packages/cascader/col.ts
@@ -1,38 +1,40 @@
 import { McComponent } from '../common/component';
+import { Weapp } from 'definitions/weapp';
 
 McComponent({
-    props: {
-        dataSource: {
-			type: Array,
-			value: []
-		},
-		value: {
-            type: String,
-            optionalTypes: [Number]
-		},
-		loading: {
-			type: Boolean,
-			value: false
-		},
-		index: {
-            type: Number,
-            observer: function(newVal, oldVal) {
-                // 滚动到初始位置
-                this.setData({
-                    contentActive: 'top'
-                });
-                // let instance = this.data.dataSource.findIndex(i => this.data.value == i.value);
-                // $(this.$el).scrollIntoView({ to: instance * 40 });
-            }
-		}
+  props: {
+    dataSource: {
+      type: Array,
+      value: []
     },
-    data: {
-        contentActive: ''
+    value: {
+      type: String,
+      optionalTypes: [Number]
     },
-    methods: {
-        handleClick(value, rowIndex) {
-			const { index: colIndex } = this.data;
-			this.$emit('change', { value, rowIndex, colIndex });
-		}
+    loading: {
+      type: Boolean,
+      value: false
+    },
+    index: {
+      type: Number,
+      observer(newVal, oldVal) {
+        // 滚动到初始位置
+        this.setData({
+          contentActive: 'top'
+        });
+        // let instance = this.data.dataSource.findIndex(i => this.data.value == i.value);
+        // $(this.$el).scrollIntoView({ to: instance * 40 });
+      }
     }
+  },
+  data: {
+    contentActive: ''
+  },
+  methods: {
+    onClick(event: Weapp.Event) {
+      const { value, rowIndex } = event.currentTarget.dataset;
+      const { index: colIndex } = this.data;
+      this.$emit('change', { value, rowIndex, colIndex });
+    }
+  }
 });
